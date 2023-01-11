b63bd1856c810dd2105019279e5b438cb07ffb6d
VasyLight <vasilda32@yandex.ru>
2023-01-11 17:32:16 +0300

    1/4 Bone Commit

---
+++ react/src/views/Signup.jsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom"
+import { useRef } from "react"
+import axiosClient from "../axios-client"
+
+export default function Signup() {
+
+  const nameRef = useRef()
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmationRef = useRef()
+
+
+
+  const onSubmit = (ev) => {
+    ev.preventDefault()
+
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value
+    }
+    //console.log(payload);
+    axiosClient.post('/signup', payload)
+      .then(({data}) => {
+        
+      })
+  }
+
+    return ( 
+        <div className="login-signup-form animated fadeInDown">
+          <div className="form">
+            <form onSubmit={onSubmit}>
+              <h1 className="title">Signup for free</h1>   
+              <input ref={nameRef} placeholder="Full name" />
+              <input ref={emailRef} type="email" placeholder="Email Address" />
+              <input ref={passwordRef} type="password" placeholder="Password" />
+              <input ref={passwordConfirmationRef} type="password" placeholder="Password Confirmation" />
+              <button className="btn btn-block">Signup</button>
+              <p className="message">
+                Already Registered? <Link to="/login">Sign in</Link>
+              </p>
+            </form>
+          </div>
+        </div>
+    )
+}
